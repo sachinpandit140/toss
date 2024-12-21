2fe0f8554d3816201f2f31fe7c90b3fdba83baab
sachinpandit140 <sachinpandit6284@gmail.com>
2024-12-21 17:30:40 -0600

    test prompt commit

--- backend/ai.py
+++ backend/ai.py
@@ -7,7 +7,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API"))
 system_prompt = (
     "Check this terms of service url and provide summary and alerts."
     "Alerts are any edge cases that are not covered by the summary or may cause problems to users."
-    "Provide sentences in order of importance."
+    "Provide sentences in order of importance and make it consice."
     "Split the summary and alerts into short sentences."
 )
 model = genai.GenerativeModel("gemini-1.5-flash", system_instruction=system_prompt)
